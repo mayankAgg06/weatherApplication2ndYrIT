c91e4a72ad22484d447e63630bd6f578865f3997
Mayank Aggarwal <56104618+mayankAgg06@users.noreply.github.com>
2025-06-23 11:37:01 +0530

    Initialised project

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const { default: axios } = require("axios");
+// import axios from 'axios';
 
-const mainParent = document.querySelector('#mainParent');
+// const mainParent = document.querySelector('#mainParent');
 
 axios.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=f0630cd7e73f0aefe4425826d32ecc8e')
+.then((response)=>{
+    console.log(response)
+})
+.catch((error)=>{
+    console.log(error)
+});
 
